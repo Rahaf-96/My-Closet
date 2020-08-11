0bc1dc95ca456a9cae80200626538e4bf2051156
rahaf-96 <rahaf.hawamdeh96@gmail.com>
2020-08-11 23:43:19 +0300

    create sign up component and implement sign up with email and password

--- src/App.js
+++ src/App.js
@@ -6,22 +6,25 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-
 import Header from './components/header/header.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
 class App extends React.Component {
   constructor() {
     super();
+
     this.state = {
       currentUser: null,
     };
   }
+
   unsubscribeFromAuth = null;
 
   componentDidMount() {
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
+
         userRef.onSnapshot((snapShot) => {
           this.setState({
             currentUser: {
@@ -29,16 +32,19 @@ class App extends React.Component {
               ...snapShot.data(),
             },
           });
+
+          console.log(this.state);
         });
-      } else {
-        this.setState({ currentUser: userAuth });
       }
+
+      this.setState({ currentUser: userAuth });
     });
   }
 
   componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
+
   render() {
     return (
       <div>
@@ -52,4 +58,5 @@ class App extends React.Component {
     );
   }
 }
+
 export default App;
