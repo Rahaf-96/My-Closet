86aed908e02ce42f1a293a8a8c79523686534e31
rahaf-96 <rahaf.hawamdeh96@gmail.com>
2020-08-21 23:57:16 +0300

    implement redux and create a reducer for user

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
+// firebase configuration
 const config = {
   apiKey: 'AIzaSyBss_ez2L9NsxC1pUDaDWCEbZPytATUXjI',
   authDomain: 'my-closet-2d234.firebaseapp.com',
@@ -12,11 +13,23 @@ const config = {
   appId: '1:148132441286:web:97ca179a6c6df35bcdbbc7',
   measurementId: 'G-RDXEZGCR89',
 };
+firebase.initializeApp(config);
+// get auth and firestore from firebase
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
 
+// configure OAuth with google and display a pop up to select an account
+const googleProvider = new firebase.auth.GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(googleProvider);
+
+// get the logged in user info from firestore
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
   const userRef = firestore.doc(`users/${userAuth.uid}`);
   const snapShot = await userRef.get();
+
+  // if the user doesn't exist create a new one
   if (!snapShot.exists) {
     const { displayName, email } = userAuth;
     const createdAt = new Date();
@@ -33,14 +46,5 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   }
   return userRef;
 };
-
-firebase.initializeApp(config);
-export const auth = firebase.auth();
-export const firestore = firebase.firestore();
-
-const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
-
-export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
